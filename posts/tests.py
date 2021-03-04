b07d4add444f2c338658127d80451c2662a94902
BlancaLobato <mardoga2010g@gmail.com>
2021-03-04 11:05:08 +0100

    Apartado 5 correccion de errores terminado

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse  #new
 from .models import Post
 
 
@@ -13,20 +13,20 @@ class PostModelTest(TestCase):
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
 
-class HomePageViewTest(TestCase):
+class HomePageViewTest(TestCase):                       # new
 
-    def setUp(self):
-        Post.objects.create(text='this is another test')
+    def setUp(self):                                    # new
+        Post.objects.create(text='this is another test') # new
 
-    def test_view_url_exists_at_proper_location(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
+    def test_view_url_exists_at_proper_location(self):  # new
+        resp = self.client.get('/')                     # new
+        self.assertEqual(resp.status_code, 200)         # new
 
-     def test_view_url_by_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
+     def test_view_url_by_name(self):                   # new
+        resp = self.client.get(reverse('home'))         # new
+        self.assertEqual(resp.status_code, 200)         # new
 
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'home.html')
+    def test_view_uses_correct_template(self):          # new
+        resp = self.client.get(reverse('home'))         # new
+        self.assertEqual(resp.status_code, 200)         # new
+        self.assertTemplateUsed(resp, 'home.html')      # new
